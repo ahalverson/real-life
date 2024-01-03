f57d57943f3ae0fe930087f451f25c67ccb6f227
SamuelKaton <102828990+SamuelKaton@users.noreply.github.com>
2024-01-02 19:25:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import nltk
 import pyotp
 import time
+
+nltk.download()
 
 totp = pyotp.TOTP('base32secret3232')
 a = totp.now() # => '492039'
@@ -8,3 +11,6 @@ a = totp.now() # => '492039'
 print(totp.verify(a)) # => True
 time.sleep(30)
 print(totp.verify(a)) # => False
+
+
+rand_words = ' '.join(sample(words.words(), n)) #n = 6
